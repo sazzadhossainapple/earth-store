47e7cc0aed14c44df1924754478d0304610f7175
sazzadhossainapple <sazzadappple81@gmail.com>
2024-07-26 23:09:09 +0600

    login added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaEyeSlash, FaEye } from 'react-icons/fa';
 import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import toast from 'react-hot-toast';
 
 const Login = () => {
     const {
@@ -11,15 +13,61 @@ const Login = () => {
         formState: { errors },
     } = useForm();
     const [showPassword, setShowPassword] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const [token, setToken] = useState('');
+    const navigate = useNavigate();
+    const location = useLocation();
 
     //password show
     const togglePassword = () => {
         setShowPassword(!showPassword);
     };
 
+    const from = location.state?.from?.pathname || '/dashboard';
+
+    useEffect(() => {
+        if (token) {
+            navigate(from, { replace: true });
+        }
+    }, [token, navigate, from]);
+
     //   login
     const onSubmit = (data) => {
-        console.log(data);
+        setLoading(true);
+
+        const user = {
+            email: data.email.trim(),
+            password: data.password.trim(),
+        };
+
+        fetch(`${import.meta.env.VITE_API_KEY_URL}/api/user/login`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                const jwtToken = data?.data?.token;
+                if (jwtToken) {
+                    Cookies.set('accessToken', jwtToken);
+                    setToken(jwtToken);
+                    toast.success('Login Successfully!');
+                    reset();
+                } else {
+                    toast.error(data?.message);
+                    console.log(data?.message);
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                toast.error(error.message);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
     };
     return (
         <div className="login-container">
@@ -55,19 +103,15 @@ const Login = () => {
                             </span>
                         )}
                         {showPassword ? (
-                            <button
+                            <FaEye
+                                className="eye-icon"
                                 onClick={togglePassword}
-                                className="border-0 bg-transparent"
-                            >
-                                <FaEye className="eye-icon" />
-                            </button>
+                            />
                         ) : (
-                            <button
+                            <FaEyeSlash
+                                className="eye-icon"
                                 onClick={(e) => setShowPassword(e)}
-                                className="border-0 bg-transparent"
-                            >
-                                <FaEyeSlash className="eye-icon" />
-                            </button>
+                            />
                         )}
                     </div>
 
@@ -75,7 +119,7 @@ const Login = () => {
                         <input
                             type="submit"
                             className="text-text-center btns w-100"
-                            value="Sign In"
+                            value={`${loading ? 'Loading...' : 'Sign In'}`}
                         />
                     </div>
                 </form>
